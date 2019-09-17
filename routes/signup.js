15e4ce260aac7d9437b11ca1b2063abcfdd43e8a
samhine <hines@elslabm-156920.science.regn.net>
2019-09-17 18:26:14 +0100

    SH: Signup route draft

--- routes/signup.js
+++ routes/signup.js
@@ -7,6 +7,12 @@ router.use(bodyParser.json());
 
 router.route(`/`).post((req, res) => {
     res.send(`Adding Todo successful`);
+    const signup = new Signup(req.body);
+    signup.save()
+        .then(signup => {
+            res.status(200).json({'signup':'signup added successfully'})
+        })
+        .catch(err => res.status(400).send(err))
 });
 
 module.exports = router;
