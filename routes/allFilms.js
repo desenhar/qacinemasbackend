f777d6ccd4eb5020dd2665dcd143a1397896b2ef
samhine <hines@elslabm-156920.science.regn.net>
2019-09-17 22:28:55 +0100

    SH: allFilms route

--- routes/allFilms.js
+++ routes/allFilms.js
@@ -3,7 +3,10 @@ const router = express.Router();
 const Films = require(`../films.model`);
 
 router.route(`/`).get((req, res) => {
-    res.send(`Getting all films`);
+    // res.send(`Getting all films`);
+    Films.find((error, films) => {
+        error ? res.status(404).send(`Not found`) : res.status(200).json(films);
+    });
 });
 
 module.exports = router;
