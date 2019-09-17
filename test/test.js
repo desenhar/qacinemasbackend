915faebdb63bf71e8fdc6aeeaedd080cc0304b32
samhine <hines@elslabm-156920.science.regn.net>
2019-09-17 14:38:51 +0100

    SH: mockdata

--- test/test.js
+++ test/test.js
@@ -3,3 +3,8 @@ const chai = require('chai');
 const chaiHttp = require('chai-http');
 const server = require('../server');
 const should = chai.should();
+
+chai.use(chaiHttp);
+describe(`Testing requirements for backend`, () => {
+
+});
