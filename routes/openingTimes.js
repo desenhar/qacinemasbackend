21701fe4282993d4dd0092ec44187497888e8b02
Mayenin <mayenino@elslabm-156923.science.regn.net>
2019-09-18 10:09:30 +0100

    OM: Opening times test passes.

--- routes/openingTimes.js
+++ routes/openingTimes.js
@@ -3,7 +3,11 @@ const router = express.Router();
 const Openings = require(`../openingtimes.model`);
 
 router.route(`/`).get((req, res) => {
-    res.send(`Getting all films`);
+    // res.send(`Getting all films`);
+    Openings.find((error, openings) => {
+        error ? res.status(404).send(`Not found`) :
+            res.status(200).json(openings);
+    });
 });
 
 module.exports = router;
