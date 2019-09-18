ef439e58d041305bb545762766b937fbd3cc2ee3
Mayenin <mayenino@elslabm-156923.science.regn.net>
2019-09-18 14:20:09 +0100

    OM: Added express validator checks to signup

--- routes/signup.js
+++ routes/signup.js
@@ -2,10 +2,28 @@ const express = require(`express`);
 const router = express.Router();
 const Signups = require(`../signups.model`);
 const bodyParser = require(`body-parser`);
+const cors = require('cors');
 
+const { check, validationResult } = require('express-validator/check');
+
+// const isoDateRegExp = /\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d\.\d+([+-][0-2]\d:[0-5]\d|Z)/;
+const isoDateRegExp = /^(?:(?:31(\/|-|\.)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(\/|-|\.)(?:0?[13-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/|-|\.)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/|-|\.)(?:(?:0?[1-9])|(?:1[0-2]))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$/
+const isoEmailRegExp = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+
+router.use(cors());
 router.use(bodyParser.json());
 
-router.route(`/`).post((req, res) => {
+router.route(`/`).post(
+    [
+        check("email").matches(isoEmailRegExp),
+        check("dob").matches(isoDateRegExp)
+    ],
+    
+    (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(422).json({ errors: errors.array() });
+        }
     // res.send(`Adding Todo successful`);
     // Instantiate new signup object
     const signup = new Signups(req.body);
