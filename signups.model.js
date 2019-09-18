ea1a413737386754a5fa3ec263d363a8a62d9050
Mayenin <mayenino@elslabm-156923.science.regn.net>
2019-09-18 12:14:47 +0100

    OM: Min length added for signups.

--- signups.model.js
+++ signups.model.js
@@ -3,8 +3,8 @@ const Schema = mongoose.Schema;
 
 const Signups = new Schema({
     "title": { type: String, required: true },
-    "firstname": { type: String, required: true },
-    "lastname": { type: String, required: true },
+    "firstname": { type: String, required: true, minlength: 2 },
+    "lastname": { type: String, required: true , minlength: 2 },
     "email": { type: String, required: true },
     "number": { type: String },
     "dob": { type: Date }
